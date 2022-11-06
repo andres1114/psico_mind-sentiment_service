539bdd7427be4fb10e6a845827b53e710c0e84eb
Andres Zapata <polako_1114@hotmail.com>
2022-11-06 15:56:02 -0500

    PSICOMIND-T-09 Changes database connection ubication

--- main.py
+++ main.py
@@ -24,13 +24,14 @@ else:
 
 #Define the constants
 current_dir_path = os.path.dirname(os.path.abspath(__file__))
-sqlite_database_filename = "psicomind.sqlite3"
+db_dir_path = "db"
+sqlite_database_filename = "automata.sqlite3"
 gs = goslate.Goslate()
 sentiment_queue_object = SentimentQueueEntity()
 sentiment_queue_status_enum = SentimentQueueStatusEnum()
 
 functions.verbose(outputMode=log_output_mode, outputMessage="[main] " + "SentimentService() " + "Enter", logName="main")
-sqlite_db_connection = DataBaseConnection(db_type='sqlite', db_host=current_dir_path + "/" + sqlite_database_filename, db_user='', db_pass='', db_name='')
+sqlite_db_connection = DataBaseConnection(db_type='sqlite', db_host=current_dir_path + "\\..\\" + db_dir_path + "\\" + sqlite_database_filename, db_user='', db_pass='', db_name='')
 
 sentiment_queue_list = sentiment_queue_object.get_sentimetn_queue(db_object=sqlite_db_connection, debug_otuput=log_output_mode)
 sentiment_queue_ready_list = []
